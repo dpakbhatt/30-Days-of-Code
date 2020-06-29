d6bd1effe8a3b5ca1121497880f253d245539b1f
dpakbhatt <deepakbhatt912@gmail.com>
2020-06-29 20:40:29 +0530

    Day 11

---
+++ Day 11/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    int arr[6][6];
+    int result;
+    int max = INT_MIN;
+    for(int i= 0; i < 6; i++){
+        for(int j = 0; j < 6; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int i=0; i < 4; i++){
+        for(int j=0; j < 4; j++){
+            result = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+            if( result > max){
+                max = result;
+            }
+        }
+    }
+    cout << max << endl;
+    return 0;
+}
