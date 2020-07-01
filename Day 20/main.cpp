d4e9597b2e88b0f0808a1c0cb50d1ca70fa74afb
dpakbhatt <deepakbhatt912@gmail.com>
2020-07-02 01:57:40 +0530

    Day 20

---
+++ Day 20/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int a_i = 0; a_i < n; a_i++){
+    	cin >> a[a_i];
+    }
+
+    int numswap = 0;
+    for(int i=0; i< n; i++){
+        for(int j = 0; j < n-1; j++){
+            if(a[j] > a[j+1]){
+                swap(a[j], a[j+1]);
+                numswap++;
+            }
+        }
+        if(numswap == 0){
+            break;
+        }
+    }
+
+    cout << "Array is sorted in " << numswap << " swaps." << endl;
+    cout << "First Element: " << a[0] << endl;
+    cout << "Last Element: " << a[a.size() -1] << endl;
+
+    return 0;
+}
